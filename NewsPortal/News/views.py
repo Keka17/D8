5da5abb9645722e180156adb8abef853041b3d4f
Victoria <vihalfblood@gmail.com>
2024-11-19 22:56:55 +0300

    Add files via upload

---
+++ NewsPortal/News/views.py
@@ -0,0 +1,168 @@
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post
+from .filters import PostFilter
+from .forms import PostForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView
+from django.shortcuts import reverse
+from django.shortcuts import redirect
+from django.contrib.auth.models import Group
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import PermissionRequiredMixin
+
+
+class PostsList(ListView):
+    model = Post
+    ordering = '-publication_date'
+    template_name = 'posts.html'
+    context_object_name = 'posts'
+    paginate_by = 10
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['post_count'] = Post.objects.count()
+        return context
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'post.html'
+    context_object_name = 'post'
+
+
+class PostSearch(ListView):
+    model = Post
+    template_name = 'search_post.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = PostFilter(self.request.GET, queryset)
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filterset'] = self.filterset
+        return context
+
+    def post(self, request, *args, **kwargs):
+        self.object_list = self.get_queryset()
+        context = self.get_context_data()
+        if len(context['posts']) == 1:
+            return redirect('post_detail', pk=context['posts'][0].pk)
+        else:
+            return self.render_to_response(context)
+
+
+class NewsCreate(CreateView, PermissionRequiredMixin):
+    permission_required = ('News.add_post',)
+    form_class = PostForm
+    model = Post
+    template_name = 'post_create.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['creating_view'] = 'news_create'
+        return context
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.news_type = 'NE'
+        post.save()
+        return redirect(post.get_absolute_url())
+
+class NewsEdit(UpdateView, PermissionRequiredMixin):
+    permission_required = ('News.change_post',)
+    form_class = PostForm
+    model = Post
+    template_name = 'post_create.html'
+
+    def get_queryset(self):
+        return super().get_queryset().filter(news_type='NE')
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.save()
+        return redirect(post.get_absolute_url())
+
+
+class NewsDelete(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('posts_list') # сюда перекинут юзера после удаления публикации
+
+    def get_queryset(self):
+        return super().get_queryset().filter(news_type='NE')
+
+class ArticlesCreate(CreateView, PermissionRequiredMixin):
+    permission_required = ('News.add_post',)
+    form_class = PostForm
+    model = Post
+    template_name = 'post_create.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['creating_view'] = 'articles_create'
+        return context
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.news_type = 'AR'
+        post.save()
+        return redirect(post.get_absolute_url())
+
+
+class ArticlesEdit(UpdateView, LoginRequiredMixin, TemplateView, PermissionRequiredMixin):
+    permission_required = ('News.change_post',)
+    form_class = PostForm
+    model = Post
+    template_name = 'post_create.html'
+
+    def get_success_url(self):
+        return reverse('articles_edit', args=[self.object.pk])
+
+    def get_queryset(self):
+        return super().get_queryset().filter(news_type='AR')
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.save()
+        return redirect(post.get_absolute_url())
+
+    # def get_object(self, queryset=None):
+    #     pk = self.kwargs.get('pk')
+    #     obj = super().get_object(queryset=queryset)
+    #
+    #     if obj.author != self.request.user:
+    #         raise Http404('У вас нет права редактировать статью чужого автора!')
+    #
+    #     return obj
+
+
+class ArticlesDelete(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('posts_list')
+
+    def get_queryset(self):
+        return super().get_queryset().filter(news_type='AR')
+
+
+class IndexView(LoginRequiredMixin, TemplateView):
+    template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['is_not_authors'] = not self.request.user.groups.filter(name='authors').exists()
+        return context
+
+
+@login_required
+def upgrade_me(request):
+    user = request.user
+    authors_group = Group.objects.get(name='authors')
+    if not request.user.groups.filter(name='authors').exists():
+        authors_group.user_set.add(user)
+    return redirect('/')
+
+
---
+++ NewsPortal/NewsPortal/settings.py
@@ -0,0 +1,134 @@
+import os.path
+from pathlib import Path
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = 'django-insecure-h2u%3+)c_bjis^b3(f)*w4-9xgixzth!1^(j#)4li)78up8yab'
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['127.0.0.1']
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'django.contrib.flatpages',
+
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
+    'allauth.socialaccount.providers.yandex',
+
+    'News',
+    'django_filters',
+]
+
+SITE_ID = 1
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
+
+
+]
+
+ROOT_URLCONF = 'NewsPortal.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.request',
+
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'NewsPortal.wsgi.application'
+
+
+AUTHENTICATION_BACKENDS = [
+       'django.contrib.auth.backends.ModelBackend',
+       'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+STATIC_URL = 'static/'
+
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
+
+
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/'
+
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_UNIQUE_EMAIL = True
+ACCOUNT_USERNAME_REQUIRED = False
+ACCOUNT_AUTHENTICATION_METHOD = 'email'
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+
+ACCOUNT_FORMS = {'signup': 'News.forms.BasicSignupForm'}
